31912ab1633960aac8079de37cc6dcbf5ad60dd8
Scott C. Reynolds <scott@scottcreynolds.com>
2016-11-10 11:31:37 -0500

    add twitter and author names

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -8,9 +8,9 @@ class SitesController < ApplicationController
 
   def get_sites
     urls = [
-      "https://medium.com/@nicolesilverberg/what-can-i-do-right-now-98169b7704b7#.7npca0tsp",
-      "http://jezebel.com/a-list-of-pro-women-pro-immigrant-pro-earth-anti-big-1788752078"
+      { url: "https://medium.com/@nicolesilverberg/what-can-i-do-right-now-98169b7704b7#.7npca0tsp", author: "Nicole Silverberg", twitter: "nsilverberg" },
+      { url: "http://jezebel.com/a-list-of-pro-women-pro-immigrant-pro-earth-anti-big-1788752078", author: "Joanna Rothkopf", twitter: "joannarothkopf" }
     ]
-    @sites = urls.map { |u| LinkThumbnailer.generate(u) }
+    @sites = urls.map { |u| { thumbnail: LinkThumbnailer.generate(u[:url]), author: u[:author], twitter: u[:twitter], url: u[:url] }  }
   end
 end
